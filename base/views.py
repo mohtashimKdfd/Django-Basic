34961ca9638a33ecf9048df70b9e95d333a16ca1
mohtashimKdfd <mohtashim.kamran@unthinkable.co>
2021-12-27 18:19:28 +0530

    Database

--- base/views.py
+++ base/views.py
@@ -2,19 +2,24 @@
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import objectss
 
-objects = [
-    {'id':1,'name':'people'},
-    {'id':2,'name':'animals'},
-    {'id':3,'name':'world'},
-    {'id':4,'name':'aliens'},
-    ]
+# objects = [
+#     {'id':1,'name':'people'},
+#     {'id':2,'name':'animals'},
+#     {'id':3,'name':'world'},
+#     {'id':4,'name':'aliens'},
+#     ]
 
 
 def home(request):
-    context = {'obj':objects}
+    objectt = objectss.objects.all()
+    context = {'obj':objectt}
     return render(request,'base/home.html',context)
 
 def index(request,pk):
-    context = {'obj':objects[int(pk)]}
+    objectt = objectss.objects.all()
+    print(type(objectt))
+    context = {'obj':objectt[int(pk)-1]}
+    # context = {'obj':objects[int(pk)-1]}
     return render(request,'base/index.html',context)
